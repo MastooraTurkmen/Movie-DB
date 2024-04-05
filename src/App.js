002ba6743828c49f17bcaeae702cb8376616d365
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-05 23:43:39 +0430

    set up reac router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Routes } from 'react-router-dom'
 
 import Home from './Home'
 import Movie from './SingleMovie'
 
 function App() {
-  return <h2>movie DB starter</h2>
+  return (
+    <Routes>
+      <Route path='/' index element={<Home />} />
+      <Route path='/movies/:id' element={<Movie />} />
+    </Routes>
+  )
 }
 
 export default App
