e1227ccd7fbf53675d99a8cd8ccb57576b64e0b7
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-06 15:49:35 +0430

    set up movie

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -3,6 +3,23 @@ import { useParams, Link } from 'react-router-dom'
 import { API_ENDPOINT } from './context'
 
 const SingleMovie = () => {
+  const { id } = useParams();
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState({
+    show: false,
+    msg: ''
+  });
+
+  const fetchMovie = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+  }
+
+  useEffect(() => {
+    fetchMovie( )
+  }, [id])
+
   return <h2>single movie</h2>
 }
 
